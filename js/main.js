4af8273083a646559650408a828e45f9d1e63a7f
Magmabender <59297838+Magmabender@users.noreply.github.com>
2020-10-20 15:19:43 -0300

    works better

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
             var nroImg = numImg[i-1]-10*Math.floor(numImg[i-1]/10.1);
             
             $("#container").append('<div class="carta" id="carta'+i+'"</div>')
-            $("#carta"+i).append('<div class="carta__cara carta__cara--front"> <img src="/img/front.svg" alt="" class="imagen"> </div>')
+            $("#carta"+i).append('<div class="carta__cara carta__cara--front" id="cartaFront'+i+'"> <img src="/img/front.svg" alt="" class="imagen"> </div>')
             $("#carta"+i).append('<div class="carta__cara carta__cara--back" id="cartaBack'+i+'"</div>')
             $("#cartaBack"+i).append('<img src="/img/img'+nroImg+'.svg" alt="" class="imagen imgBack img'+nroImg+'">')    
         }
@@ -30,35 +30,48 @@ $(document).ready(function() {
     }
 
     function flip() {
-        var clases = [];
-        var cartas = [];
-        var cartaId = [];
+        let clases = [];
+        let cartaBack = [];
+        let cartaFront = [];
+        let cartaId = [];
+        let delayedFlip;
+        let delayedFade;
         $('.carta').click(function () {
-            
+            cantidad+=1;
             if (cantidad<2) {
-                cartas[cantidad] = $(this).attr("id");
-                clases[cantidad] = $(this).find(".imgBack").attr('class');
-                cartaId[cantidad] = $(this).attr('id');
+                clearTimeout(delayedFlip);
+                $('.carta').removeClass("is-flipped")
+                cartaFront[cantidad-1] = $(this).find(".carta__cara--front").attr("id");
+                cartaBack[cantidad-1] = $(this).find(".carta__cara--back").attr("id");
+                clases[cantidad-1] = $(this).find(".imgBack").attr('class');
+                cartaId[cantidad-1] = $(this).attr('id');
                 $(this).toggleClass("is-flipped");
-                cantidad+=1;
             }else if (cantidad==2){
+                cartaFront[cantidad-1] = $(this).find(".carta__cara--front").attr("id");
+                cartaBack[cantidad-1] = $(this).find(".carta__cara--back").attr("id");
+                clases[cantidad-1] = $(this).find(".imgBack").attr('class');
+                cartaId[cantidad-1] = $(this).attr('id');
+                $(this).toggleClass("is-flipped");
                 if (check(clases,cartaId)){
-                    //$("#"+cartas[0]).fadeOut("slow")
-                    //$("#"+cartas[1]).fadeOut("slow")
-                    $("#"+cartas[0]).css('opacity','0');
-                    $("#"+cartas[1]).css('opacity','0');
+                    clearTimeout(delayedFlip);
+                    $("#"+cartaBack[0]).css('opacity','0');
+                    $("#"+cartaBack[1]).css('opacity','0');
+                    $("#"+cartaFront[0]).css('opacity','0');
+                    $("#"+cartaFront[1]).css('opacity','0');
                     cantidad=0;
                 } else {
-                    $('.carta').removeClass("is-flipped")
-                    cantidad=0;
+                    delayedFlip = setTimeout(function(){
+                        $('.carta').removeClass("is-flipped")
+                    },1500)
+                    cantidad=0; 
                 }
-            }else {
-                $('.carta').removeClass("is-flipped")
-                cantidad=0;
-            } 
+            }
         })
     }
     
+    
+
+
     function check(val1,val2) {
         if ((val1[0]==val1[1]) && (val2[0] != val2[1])) {
             return true
